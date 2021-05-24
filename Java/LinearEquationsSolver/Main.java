c9f5c447793b111125834e7b292fd94da2400f4f
TheOfficer2303 <76702547+TheOfficer2303@users.noreply.github.com>
2021-05-24 17:43:57 +0200

    Update Main.java

--- Java/LinearEquationsSolver/Main.java
+++ Java/LinearEquationsSolver/Main.java
@@ -1,10 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
-
-
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
@@ -12,42 +5,28 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Absolute path to the input file: ");
         String file = scanner.nextLine();
+
         File inFile = new File(file);
         File outFile = new File("C:\\Users\\blazb\\Desktop\\out.txt");
-        boolean var4 = true;
 
-        try {
-            scanner = new Scanner(inFile);
+        scanner = new Scanner(inFile);
+        int n = Integer.parseInt(scanner.nextLine());
+        generate(n, scanner, outFile);
 
-            try {
-                int n = Integer.parseInt(scanner.nextLine());
-                generate(n, scanner, outFile);
-            } catch (Throwable var9) {
-                try {
-                    scanner.close();
-                } catch (Throwable var8) {
-                    var9.addSuppressed(var8);
-                }
-
-                throw var9;
-            }
-
-            scanner.close();
-        } catch (FileNotFoundException var10) {
-            System.out.println("Error! FileNotFound!");
-        }
+        scanner.close();
 
     }
 
-    public static void generate(int n, Scanner scanner, File outFile) {
+
+    public static void generate(int n, Scanner scanner, File outFile) throws FileNotFoundException {
         double[][] matrix = new double[n][n + 1];
 
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < n + 1; ++j) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n + 1; ++j) {
                 matrix[i][j] = scanner.nextDouble();
             }
         }
@@ -55,17 +34,17 @@ public class Main {
         solveSystem(n, matrix, outFile);
     }
 
-    public static void solveSystem(int n, double[][] matrix, File outFile) {
+    public static void solveSystem(int n, double[][] matrix, File outFile) throws FileNotFoundException {
         row(n, matrix);
         printSolutionsInFile(n, matrix, outFile);
         printSolutionsOnTerminal(n, matrix);
     }
 
     public static void row(int n, double[][] matrix) {
-        for(int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i) {
             double mainNum = matrix[i][i];
             if (mainNum != 0.0D) {
-                for(int j = 0; j < n + 1; ++j) {
+                for (int j = 0; j < n + 1; ++j) {
                     matrix[i][j] /= mainNum;
                 }
             }
@@ -76,12 +55,12 @@ public class Main {
     }
 
     public static void column(int n, double[][] matrix) {
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < n; ++j) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
                 if (j != i) {
                     double mainNum = matrix[j][i];
 
-                    for(int k = 0; k < n + 1; ++k) {
+                    for (int k = 0; k < n + 1; ++k) {
                         matrix[j][k] -= mainNum * matrix[i][k];
                     }
                 }
@@ -91,34 +70,17 @@ public class Main {
     }
 
     public static void printSolutionsOnTerminal(int n, double[][] matrix) {
-        for(int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i) {
             System.out.printf(Locale.US, "%.2f\n", matrix[i][n]);
         }
 
     }
 
-    public static void printSolutionsInFile(int n, double[][] matrix, File outFile) {
-        try {
-            PrintWriter fileWriter = new PrintWriter(outFile);
-
-            try {
-                for(int i = 0; i < n; ++i) {
-                    fileWriter.printf(Locale.US, "%.2f\n", matrix[i][n]);
-                }
-            } catch (Throwable var7) {
-                try {
-                    fileWriter.close();
-                } catch (Throwable var6) {
-                    var7.addSuppressed(var6);
-                }
-
-                throw var7;
-            }
-
-            fileWriter.close();
-        } catch (FileNotFoundException var8) {
-            var8.printStackTrace();
+    public static void printSolutionsInFile(int n, double[][] matrix, File outFile) throws FileNotFoundException {
+        PrintWriter fileWriter = new PrintWriter(outFile);
+        for (int i = 0; i < n; ++i) {
+            fileWriter.printf(Locale.US, "%.2f\n", matrix[i][n]);
         }
-
+        fileWriter.close();
     }
 }
